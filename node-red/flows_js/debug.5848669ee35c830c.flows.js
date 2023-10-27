13961c0b619a678735e440f0c99c649c00f18b12
Søren Thomassen <115066094+st-randers@users.noreply.github.com>
2023-10-27 10:49:40 +0000

    Added debugs to SQL insert and new data std out

---
+++ node-red/flows_js/debug.5848669ee35c830c.flows.js
@@ -0,0 +1,20 @@
+const Node = {
+  "id": "5848669ee35c830c",
+  "type": "debug",
+  "z": "6f89a22d2f227f45",
+  "name": "SQL",
+  "active": true,
+  "tosidebar": true,
+  "console": true,
+  "tostatus": false,
+  "complete": "sql",
+  "targetType": "msg",
+  "statusVal": "",
+  "statusType": "auto",
+  "x": 1000,
+  "y": 1320,
+  "wires": [],
+  "_order": 71
+}
+
+module.exports = Node;
